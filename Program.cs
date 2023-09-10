153e46ae71c6568a97e6588cb11179fcc171ca9a
Stefeson <stefesongomesdealmeidanetoo@gmail.com>
2023-09-10 16:05:55 -0300

    troca de variaveis

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Stefeson
+Console.Clear();
+
+﻿string nome, nome2, nome3;
+
+Console.Write("Primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Sobrenome: ");
+nome2 = Console.ReadLine()!;
+
+nome3 = nome;
+nome = nome2;
+nome2 = nome3;
+
+Console.Clear();
+
+Console.WriteLine("Trocando os nomes");
+Console.WriteLine($"Nome = {nome}");
+Console.WriteLine($"Sobrenome = {nome2}");
